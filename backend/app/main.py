981ac72b6696b66a665b12abda65238927770dd5
mintaewon <taewon1230@naver.com>
2022-08-02 21:53:43 +0900

    백엔드 수정

---
+++ backend/app/main.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Data(BaseModel):
+    name : str
+    num : int
+
+db = []
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "welcome"
+
+@app.get("/info/")
+async def get_info():
+    ls = []
+    print(db)
+    for i in db:
+        ls.append({'name':i['name'], 'use':i['use'], 'date':i['date']})
+    return ls
+
+@app.post("/info/")
+async def create_info(data:Data):
+    d = [0]*16
+    df = data.dict()
+    d[df['num']] = 1
+    df['use'] = d
+    df['date'] = str(datetime.now())
+    db.append(df)
+    return db[-1]
