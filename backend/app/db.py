6d7480d64ef293f0cfd023b473b7bf6e446b12a8
mintaewon <taewon1230@naver.com>
2022-08-13 01:06:46 +0900

    feat: 데이터 수집 방법 변경 완료

--- backend/app/db.py
+++ backend/app/db.py
@@ -15,8 +15,7 @@ conn = pymysql.connect(
 curs = conn.cursor(pymysql.cursors.DictCursor)
 
 # ==== insert ====
-sql = """insert into TIMELINE(datetime, weather, Freeweight, SmithMachine, PowerLegPress, PowerRack, InclineBench, FlatBench, ShoulderPressMachine, Cable, LatPulldown, ChestFlyMachine, ChestPressMachine, SeatedRowMachine, LegPressMachine, InnerThighMachine, LegExtension, LegCurl)
-         values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+sql = """insert into USEDATA values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
 def insert_data(df_row:tuple):
     curs.execute(sql, df_row)
